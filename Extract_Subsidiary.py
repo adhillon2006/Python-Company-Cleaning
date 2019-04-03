65e440a6477ef949833855167b39df740a61b844
adhillon2006 <adhillon2006@gmail.com>
2019-04-03 00:08:27 -0700

    Create Extract_Subsidiary.py

---
+++ Extract_Subsidiary.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import wikipedia as wp
+
+Top_Company = ['Alphabet','Apple']
+
+for company in range(0,len(Top_Company)):
+    html = wp.page("List of mergers and acquisitions by "+ Top_Company[1]).html().encode("UTF-8") # Get the html source
+    df = pd.read_html(html)[0]
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = new_header
+    df['Top Company'] = Top_Company[company]
