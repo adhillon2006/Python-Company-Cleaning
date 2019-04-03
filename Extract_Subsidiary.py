d767fabdd631c2a15d0df0a09a69329250e2fc2f
adhillon2006 <adhillon2006@gmail.com>
2019-04-03 08:51:18 -0700

    Update Extract_Subsidiary.py

--- Extract_Subsidiary.py
+++ Extract_Subsidiary.py
@@ -4,7 +4,7 @@ import wikipedia as wp
 Top_Company = ['Alphabet','Apple']
 
 for company in range(0,len(Top_Company)):
-    html = wp.page("List of mergers and acquisitions by "+ Top_Company[1]).html().encode("UTF-8") # Get the html source
+    html = wp.page("List of mergers and acquisitions by "+ Top_Company[company]).html().encode("UTF-8") # Get the html source
     df = pd.read_html(html)[0]
     new_header = df.iloc[0]
     df = df[1:]
